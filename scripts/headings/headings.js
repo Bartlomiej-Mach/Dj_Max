cbd1865f883e1b478e09cd60595a9a6779477f20
unknown <bartekmachu@gmail.com>
2021-11-21 12:53:39 +0100

    add webicon and OG: headings

--- scripts/headings/headings.js
+++ scripts/headings/headings.js
@@ -2,5 +2,14 @@ document.write(`
 
 <link href="https://fonts.googleapis.com/css2?family=Blinker:wght@200;600&family=Fjalla+One&family=Rock+Salt&family=Spartan:wght@400;700&display=swap" rel="stylesheet">
 
+<!-- webicon -->
+<link rel="icon" href="./assets/logo/webicon.png">
+
+<meta property="og:title" content="DJ Maks - twój wodzirej weselny" />
+<meta property="og:image" content="./assets/baner/baner.png" />
+<meta property="og:description" content="Zbliża się twoja impreza weselna i jeszcze nie masz muzyki? Nie mogłeś trafić lepiej, sprawię żeby twoja impreza była niezapomniana!" />
+
+<meta name="keywords" content="hale magazynowe, magazyn, magazyny, hale produkcyjne olkusz, hala, zarządca nieruchomości, chłodnie, hala produkcyjna, wynajem hal, hale na wynajem, biura, olkusz, małopolskie, śląskie">
+<meta name="description" content="DJ Maks - twój wodzirej weselny" /> 
 
 `);
